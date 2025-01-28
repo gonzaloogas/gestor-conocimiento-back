cbc3df862a3f438cdae6de2cfa7c812edc6c97d7
Diego Aguirre <diego.aguirrevalenzuela@gmail.com>
2025-01-28 17:23:38 -0300

    integracion keycloak parte 1

---
+++ src/main/java/com/getronics/gestor_conocimiento_back/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.getronics.gestor_conocimiento_back.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+//para obligar que JwtAuthenticationConverter nunca sea nulo
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Autowired
+    private  JwtAuthenticationConverter jwtAuthenticationConverter;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(http -> http.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth -> {
+                    oauth.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter));
+                })
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+
+    }
+}
--- src/main/java/com/getronics/gestor_conocimiento_back/controller/ProfesionalController.java
+++ src/main/java/com/getronics/gestor_conocimiento_back/controller/ProfesionalController.java
@@ -10,6 +10,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,26 +27,31 @@ public class ProfesionalController {
     private ProfesionalHabilidadService habilidadService;
 
     @PostMapping("/crear")
+    @PreAuthorize("hasRole('profesional_client_role') or hasRole('administrador_client_role')")
     public ResponseEntity<Profesional> crearProfesional(@Valid @RequestBody Profesional profesional) throws DataNotFoundException {
         return new ResponseEntity<>(profesionalService.crearProfesional(profesional), HttpStatus.CREATED);
     }
 
     @PutMapping("/actualizar/{id}")
+    @PreAuthorize("hasRole('profesional_client_role') or hasRole('administrador_client_role')")
     public ResponseEntity<Profesional> actualizarProfesional(@Valid @RequestBody Profesional profesional,@PathVariable Long id) throws DataNotFoundException {
         return new ResponseEntity<>(profesionalService.actualizarProfesional(profesional, id),HttpStatus.OK);
     }
 
     @GetMapping("/listar/{id}")
+    @PreAuthorize("hasRole('profesional_client_role') or hasRole('administrador_client_role') or hasRole('operador_client_role')")
     public ResponseEntity<Optional<Profesional>> listarProfesionalPorId(@PathVariable("id") Long id) throws DataNotFoundException {
         return new ResponseEntity<>(profesionalService.listarProfesionalPorId(id),HttpStatus.OK);
     }
 
     @GetMapping("/listarTodos")
+    @PreAuthorize("hasRole('operador_client_role') or hasRole('administrador_client_role')")
     public ResponseEntity<List<Profesional>> listarProfesional(){
         return new ResponseEntity<>(profesionalService.listarProfesionales(),HttpStatus.OK);
     }
 
     @DeleteMapping("/eliminar/{id}")
+    @PreAuthorize("hasRole('administrador_client_role')")
     public ResponseEntity<String> eliminarProfesionalPorId(@PathVariable("id") Long id){
         profesionalService.eliminarProfesionalPorId(id);
         return new ResponseEntity<>("Profesional eliminado con exito",HttpStatus.OK);
